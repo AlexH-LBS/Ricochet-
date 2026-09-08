a1873fb427c1105d3fe36d67f3ec72f74dbfaa9e
AlexH-LBS <alexander.holmstedt@elev.ga.lbs.se>
2026-09-08 10:53:12 +0200

    Start on Camera Control

--- Ricochet/Assets/Scripts/PlayerCamera.cs
+++ Ricochet/Assets/Scripts/PlayerCamera.cs
@@ -1,16 +1,41 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float sensitivity;
+    public float x;
+    public float y;
+    public DeltaControl delta;
+
+    Transform xRotation;
+    Vector2 lookInput;
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        //Temp setup, see how the values work
+        //x + delta.x;
+
+        Debug.Log(delta);
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+
+    void HandleMouseLook()
+    {
+        //make own, euler is 3 inputs of rotation xyz. eq for z, mouse for xy, clamp y -89,89. clamp z -45,45. use deltacontrol (for mouse speed)
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
